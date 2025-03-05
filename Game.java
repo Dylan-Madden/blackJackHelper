49762de986906393a3e99eea8d45aadb003f0132
Dylan-Madden <79376614+Dylan-Madden@users.noreply.github.com>
2025-03-05 11:46:10 -0500

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,13 @@
+public class Game {
+    public Game() {
+        // Calls a superclass’s no-argument constructor
+    }
+
+    public void method() {
+        System.out.print("start");
+    }
+
+    public void gameMethod() {
+        System.out.print("loading...");
+    }
+}
